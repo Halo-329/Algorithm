74f0a853c2aa65113a41c20025ff6500395cb7b7
halo <dongjin778@naver.com>
2025-04-03 09:54:32 +0900

    백준 11066 파일 합치기

--- ETC/test.py
+++ ETC/test.py
@@ -1,3 +1,8 @@
-from queue import PriorityQueue
+N=4
 
-que=PriorityQueue()
+for k in range(0,N-1):
+    for i in range(0,N-k-1):
+        j=i+k+1
+        print(f"({i},{j})", end=" ")
+    print()
+    
