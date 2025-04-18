4748cb2deff7a96ec02cd2d4b5eb2677991d044c
내 이름 <you@example.com>
2025-04-18 16:04:30 +0900

    1697 s1

--- 2_Algorithms/1_Road_Map/1_Graph_Traversal/1_BFS/2606.py
+++ 2_Algorithms/1_Road_Map/1_Graph_Traversal/1_BFS/2606.py
@@ -1,33 +1,34 @@
 import sys
 from collections import deque
 
-com_num = int(sys.stdin.readline())
+node_cnt = int(sys.stdin.readline())
 edge_cnt = int(sys.stdin.readline())
 
-graph = [[0] * (com_num + 1) for _ in range(com_num + 1)]
+graph = [[0] * (node_cnt + 1) for _ in range(node_cnt + 1)]
 for _ in range(edge_cnt):
     x, y = map(int, sys.stdin.readline().split())
     graph[x][y] = 1
     graph[y][x] = 1
     
 
-vst = [0] * (com_num + 1)
+vst = [0] * (node_cnt + 1)
 
-cnt=0
+cnt=-1
 que=deque([])
 def BFS(num):
     global cnt
     que.append(num)
-
+    vst[num]=1
+    
     while que:
-        n_com_num=que.popleft()    
-        vst[n_com_num]=1
+        now_node=que.popleft()    
         cnt+=1
-        for i in range(1,com_num+1):
-            if graph[n_com_num][i] == 1 and vst[i]==0:
+        for i in range(1,node_cnt+1):
+            if graph[now_node][i] == 1 and vst[i]==0:
                 que.append(i)
+                vst[i]=1
                 
-BFS(0)
+BFS(1)
 print(cnt)
                 
 
