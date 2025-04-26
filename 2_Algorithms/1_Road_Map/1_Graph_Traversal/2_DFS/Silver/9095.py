2efffd6e14ae50d6be865cf078e722bbd4716fb8
내 이름 <you@example.com>
2025-04-26 20:20:21 +0900

    d

--- 2_Algorithms/1_Road_Map/1_Graph_Traversal/2_DFS/Silver/9095.py
+++ 2_Algorithms/1_Road_Map/1_Graph_Traversal/2_DFS/Silver/9095.py
@@ -1,5 +1,9 @@
 import sys
 
+'''
+https://velog.io/@halo_3735/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-9095-123-%EB%8D%94%ED%95%98%EA%B8%B0
+'''
+
 T = int(sys.stdin.readline())
 
 dp = [0] * (12)
