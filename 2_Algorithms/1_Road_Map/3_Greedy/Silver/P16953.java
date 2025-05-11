1087480625075795376de03caa388a699706d4d8
내 이름 <you@example.com>
2025-05-11 15:41:06 +0900

    구조편집

--- 2_Algorithms/1_Road_Map/3_Greedy/Silver/P16953.java
+++ 2_Algorithms/1_Road_Map/3_Greedy/Silver/P16953.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
+public class P16953 {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
