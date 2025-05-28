ec135811ad5cfd6f83cab05cd45ab66fb3289d8b
내 이름 <you@example.com>
2025-05-28 12:01:51 +0900

    [ADD] backjoon 11723 sol

--- ETC/test.java
+++ ETC/test.java
@@ -1,11 +1,18 @@
-import java.sql.Array;
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 
 public class test {
-    public static void main(String args[]) {
-        String a = "456612";
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        bw.write(st.nextToken());
+
+        bw.flush();
+        bw.close();
 
     }
 }
-
