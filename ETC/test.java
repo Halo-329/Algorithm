fcf0a6a4eca23f9303e7a3ac3a78a638b12ced4c
내 이름 <you@example.com>
2025-05-29 09:24:35 +0900

    [Add] backjoon 1475 sol

--- ETC/test.java
+++ ETC/test.java
@@ -4,15 +4,7 @@ import java.lang.*;
 
 public class test {
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine(), " ");
-        bw.write(st.nextToken());
-
-        bw.flush();
-        bw.close();
+        System.out.println();
 
     }
 }
