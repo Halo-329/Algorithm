b8a8902037eec4b6452667ce39dfa3fa14204fd0
halo <dongjin778@naver.com>
2025-06-09 10:04:03 +0900

    [FIX] 폴더 구조 변경 1. 자료구조와 알고리즘으로 먼저 폴더구조를 변경 2. 각 폴더별 이론과 문제들로 구분

--- ETC/test.java
+++ ETC/test.java
@@ -3,6 +3,25 @@ import java.io.*;
 import java.lang.*;
 
 public class test {
+
+    class Edge implements Comparable<Edge> {
+        int n;
+        int m;
+        int weight;
+
+        Edge(int n, int m, int weight) {
+            this.n = n;
+            this.m = n;
+            this.weight = n;
+        }
+
+
+        @Override
+        public int compareTo(Edge e) {
+            return this.weight - e.weight;
+        }
+    }
+
     public static void main(String args[]) throws IOException {
         String a = "";
 
---
+++ algorithms/8_MST/problems/P9372.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class P9372 {
+
+
+    public static void main(String args[]) throws IOException {
+        String a = "";
+
+        System.out.println(a.isEmpty());
+
+    }
+}
