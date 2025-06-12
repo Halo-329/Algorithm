4bd48239577960d0389bf6e42214927ceb17ca6f
halo <dongjin778@naver.com>
2025-06-12 11:33:30 +0900

    [Add] 백준 1774 우주신과의 교감 문제 해결

--- ETC/test.java
+++ ETC/test.java
@@ -4,8 +4,19 @@ import java.lang.*;
 
 public class test {
 
-    static double GetDistance(P4386.Node a, P4386.Node b) {
-        return Math.sqrt(Math.pow(Math.abs(a.x - b.x), 2) + Math.pow(Math.abs(a.y - b.y), 2));
+//    static double GetDistance(P4386.Node a, P4386.Node b) {
+//        return Math.sqrt(Math.pow(Math.abs(a.x - b.x), 2) + Math.pow(Math.abs(a.y - b.y), 2));
+//    }
+
+
+    static double getDistance(P1774.Node v1, P1774.Node v2) {
+        int x = v1.x - v2.x;
+        int y = v1.y - v2.y;
+
+        double pow_x = Math.pow(x, 2);
+        double pow_y = Math.pow(y, 2);
+
+        return Math.sqrt(pow_x + pow_y);
     }
 
     public static void main(String args[]) throws IOException {
